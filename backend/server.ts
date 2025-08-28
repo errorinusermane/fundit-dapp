8228f4bbfc59812406a333d83a19aa62059bd8c8
Susie <thecatver@gmail.com>
2025-08-29 04:06:17 +0900

    ci: 서버 포트 번호 수정

--- backend/server.ts
+++ backend/server.ts
@@ -33,7 +33,7 @@ app.use("/token", tokenRouter);
 app.use("/auth", authRouter);
 
 // ✅ 서버 시작
-const PORT = Number(process.env.PORT) || 8080;
+const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
